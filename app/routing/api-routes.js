8f4698e0a9c8bce976c93cb19649da28bb5e0b0b
gsgallant <greg.gallant@yahoo.com>
2016-04-29 11:45:43 -0400

    fixed errant logic

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -20,41 +20,51 @@ module.exports = function(app){
 	app.post('/api/survey', function(req, res){
 		
 		friendsData.push(req.body);
-		userScores=req.body.scores;
-		
-		
-		compatibilityArray =[];
-		
-		for(friendNum=0;friendNum<friendsData.length-2;++friendNum){
+		// jsonFriendsData = JSON.stringify(friendsData);
+		userScores = req.body.scores;
+		console.log("friendsData.length= %s",friendsData.length);
+		//console.log("userScores: "+userScores);
+		compatibilityArray = [];
+		for(friendNum=0;friendNum<friendsData.length-1;++friendNum){
 			var compatibilityScore=0;
 			var scoreDiff=0;
 			for(i=0;i<userScores.length-1;++i){
+				// console.log("userScores[i]: " + userScores[i]);
+				// console.log("friendsData.score[i]:"+friendsData[friendNum].scores[i]);
 				scoreDiff=Math.abs(userScores[i]-friendsData[friendNum].scores[i]);
+				// console.log("scoreDiff="+scoreDiff);
 				compatibilityScore = compatibilityScore + scoreDiff;
+				// console.log("compatibilityScore="+compatibilityScore);
 			}
-
-			// console.log("Friend# "+ friendNum+ " score: "+ compatibilityScore);
-			compatibilityArray.push(compatibilityScore);
+			//this array represents the score and also the position of the best friend in the friendsarray.
+			//using this type of assignment meant eliminating an if statement testing for the first entry in friendsarray
+			compatibilityArray[friendNum]=compatibilityScore;
 		}
-		// console.log(compatibilityArray);
+		
+		console.log("compatibility array: "+compatibilityArray);
 		//find the best friend match.
 		var bestFriendIndex = 0;
-		var bestFriendScore = compatibilityArray[bestFriendIndex];
-		for(i=1;i<compatibilityArray.length-1;++i){
+		var bestFriendScore = compatibilityArray[0];
+		console.log("compatibilityArray.length=%s",compatibilityArray.length);
+		for(i=1;i<compatibilityArray.length;++i){
+			console.log("compatibilityArray["+i+"]="+compatibilityArray[i]);
 			if(compatibilityArray[i]<bestFriendScore){
 				bestFriendIndex = i;
 				bestFriendScore = compatibilityArray[i];
 			}
 		}
-		var bestFriendName = friendsData[bestFriendIndex].name;
-		var bestFriendPhotoLink = friendsData[bestFriendIndex].photo;
 		
-		// console.log(compatibilityArray);
-		// console.log("bestFriendIndex: "+bestFriendIndex);
-		// console.log("bestFriendScore: "+bestFriendScore);
-		// console.log("Name:%s",bestFriendName);
-		// console.log("PhotoLink: ",bestFriendPhotoLink);
-		// console.log(data);
+		console.log("friendsData stringify'd: "+JSON.stringify(friendsData));
+		var bestFriendName = friendsData[bestFriendIndex].friendName;
+		var bestFriendPhotoLink = friendsData[bestFriendIndex].friendPhoto;
+		if(bestFriendIndex==friendsData.length-1){
+			bestFriendName = "You are your own best friend!";
+		}
+		//console.log(compatibilityArray);
+		console.log("bestFriendIndex: "+ bestFriendIndex);
+		console.log("bestFriendScore: "+ bestFriendScore);
+		console.log("Name:%s",bestFriendName);
+		console.log("PhotoLink: ",bestFriendPhotoLink);
 		
 		res.json({
 			'name':bestFriendName,
