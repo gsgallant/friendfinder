63790da1ab14c03a9cbd5a28f70f57de7d68321d
gsgallant <greg.gallant@yahoo.com>
2016-04-29 21:32:28 -0400

    cleanup console.logs

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,4 @@
 //sample data for testing.
 var friendsArray = [{"friendName":"dog","friendPhoto":"http://animaliaz-life.com/data_images/dog/dog3.jpg","scores":["1","1","1","1","1","1","1","1","1","1"]},{"friendName":"cat","friendPhoto":"https://www.petfinder.com/wp-content/uploads/2012/11/140272627-grooming-needs-senior-cat-632x475.jpg","scores":["5","5","5","5","5","5","5","5","5","5"]},{"friendName":"monkey","friendPhoto":"http://pic.1fotonin.com/data/wallpapers/25/WDF_729546.jpg","scores":["4","4","4","4","4","4","4","4","4","4"]}];
-
 // export the array. This makes it accessible to other files using require. 
 module.exports = friendsArray; 
